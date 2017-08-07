f44592b3272c9ee643044f2d9bda27e7e0434431
dark19940411 <279714010@qq.com>
2017-08-07 17:50:35 +0800

    添加进度条

--- Package.swift
+++ Package.swift
@@ -8,5 +8,6 @@ let package = Package(
         .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1"),
         .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
+        .Package(url: "https://github.com/jkandzi/Progress.swift", majorVersion: 0)
     ]
 )
--- Sources/FiberDownload.swift
+++ Sources/FiberDownload.swift
@@ -8,18 +8,30 @@
 
 import Foundation
 import Alamofire
+import Progress
 
-struct FiberDownload {
+class FiberDownload {
     /// Singleton of FiberDownload
     static let `default` = FiberDownload()
     
+    /// A semaphore to avoid this script being terminated from asynchronous task
     fileprivate let sema = DispatchSemaphore(value: 0)
+    /// Because the actual applying queue of Alamorefire can't be got, and the main queue is blocked by semaphore while the downloading task is executing, FiberDownload has to possess its own queue to handle call-back
     fileprivate let handlerQueue = DispatchQueue(label: "com.turtle.Fiber.downloadHandlerQueue")
+    /// The progress bar of Fiber
+    fileprivate var bar = ProgressBar(count: 101, configuration: [ProgressPercent(), ProgressBarLine(barLength: 60)])
     
-    fileprivate init() {
-        
-    }
+    /// A downloaded count down(percentage), which is in [0, 100] (mathematic interval)
+    fileprivate var currentProgress: Int = 0
     
+    
+    fileprivate init() {}
+    
+    /// Start download job with a sourceURL and a Destination URL
+    ///
+    /// - Parameters:
+    ///   - sourceURL: The URL stands for the resources' place
+    ///   - destURL: The URL stands for the place where downloaded file to be
     public func startDownload(from sourceURL: URL, to destURL: URL) {
         let destination: DownloadRequest.DownloadFileDestination = { _, _ in
             return (destURL, [.removePreviousFile, .createIntermediateDirectories])
@@ -28,18 +40,30 @@ struct FiberDownload {
         let downloadRequest = Alamofire.download(sourceURL, to: destination)
         
         downloadRequest.response(queue: handlerQueue) { response in
-            print(response)
-            //            if response.error == nil, let resource = response.destinationURL?.path {
-            //
-            //            }
+            if response.error == nil, let resource = response.destinationURL?.path {
+                
+            }
             
             self.sema.signal()
         }
         
         downloadRequest.downloadProgress(queue: handlerQueue) { progress in
-            print("progress: \(progress.completedUnitCount / progress.totalUnitCount)")
+            self.updateProgressBar(with: progress.fractionCompleted)
         }
+        bar.next()
         
         sema.wait()
+    }
+    
+    fileprivate func updateProgressBar(with progress: Double) {
+        
+        let downloadedFrac = Int(floor(progress * 100))
+        let progressSubstract = downloadedFrac - currentProgress
+        print("progress: \(progress)\n downloadedFrac: \(downloadedFrac)\ncurrentProgress: \(currentProgress)")
+        
+        for _ in 0..<progressSubstract {
+            bar.next()
+            currentProgress = downloadedFrac
+        }
     }
 }
